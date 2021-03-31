7632c524b1f897187c9614b8b624657db96dafb3
Renan Cunha <renan.cunha20@gmail.com>
2021-03-31 09:07:24 -0300

    Limpando imports não utilizados.

--- src/main/java/grupozap/engzapchallengerestapi/config/SwaggerConfig.java
+++ src/main/java/grupozap/engzapchallengerestapi/config/SwaggerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -18,7 +17,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.ArrayList;
-
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
@@ -56,11 +54,5 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
         return apiInfo;
     }
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+
 }
